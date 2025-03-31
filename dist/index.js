a4f903f32625633fe6a9f4d29edb915662e9660c
Jagdish Parihar <jatin6972@gmail.com>
2025-03-31 09:28:40 +0530

    inital ai setup

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,7 @@
 import { generateText } from "ai";
 import { openai } from "@ai-sdk/openai";
+import dotenv from "dotenv";
+dotenv.config();
 const result = await generateText({
     model: openai("gpt-4o"),
     prompt: "Hello, world!",
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,44 @@
-import { generateText } from "ai"
+import { generateText, pipeDataStreamToResponse, streamText } from "ai"
 import { openai } from "@ai-sdk/openai"
+import dotenv from "dotenv"
+import { GmxSdk } from "@gmx-io/sdk";
 
-const result = await generateText({
-  model: openai("gpt-4o"),
-  prompt: "Hello, world!",
-})
 
-console.log(result)
+dotenv.config()
+
+import { createServer } from 'http';
+
+createServer(async (req, res) => {
+  switch (req.url) {
+    case '/': {
+      const result = streamText({
+        model: openai('gpt-4o'),
+        prompt: 'Invent a new holiday and describe its traditions.',
+      });
+      result.pipeDataStreamToResponse(res);
+      break;
+    }
+
+    case '/stream-data': {
+      // immediately start streaming the response
+      pipeDataStreamToResponse(res, {
+        execute: async dataStreamWriter => {
+          dataStreamWriter.writeData('initialized call');
+
+          const result = streamText({
+            model: openai('gpt-4o'),
+            prompt: 'Invent a new holiday and describe its traditions.',
+          });
+
+          result.mergeIntoDataStream(dataStreamWriter);
+        },
+        onError: error => {
+          // Error messages are masked by default for security reasons.
+          // If you want to expose the error message to the client, you can do so here:
+          return error instanceof Error ? error.message : String(error);
+        },
+      });
+      break;
+    }
+  }
+}).listen(8080);
